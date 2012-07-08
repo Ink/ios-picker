0a41f562e72483f74aca987de28557be62233b34
liyanchang <liyanchang@mit.edu>
2012-07-08 16:33:52 -0700

    text editing app

---
+++ FilepickerDemoText/FilepickerDemoText/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  FilepickerDemoText
+//
+//  Created by Liyan David Chang on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FPPicker/FPPicker.h>
+
+@interface ViewController : UIViewController <UITextViewDelegate, FPPickerDelegate, FPSaveDelegate>
+
+@property IBOutlet UITextView *textView;
+
+@end
---
+++ FilepickerDemoText/FilepickerDemoText/ViewController.m
@@ -0,0 +1,146 @@
+//
+//  ViewController.m
+//  FilepickerDemoText
+//
+//  Created by Liyan David Chang on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize textView = _textView;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    NSLog(@"begin edit");
+    _textView.frame = CGRectMake(0, 44, self.view.frame.size.width, self.view.frame.size.height-216-44);
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    NSLog(@"end edit");
+    _textView.frame = CGRectMake(0, 44, self.view.frame.size.width, self.view.frame.size.height);
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {   
+    [textField resignFirstResponder]; 
+}
+
+
+
+- (IBAction)pickerAction: (id) sender {
+    NSLog(@"Picking");
+    /*
+     * Create the object
+     */
+    FPPickerController *fpController = [[FPPickerController alloc] init];
+    
+    /*
+     * Set the delegate
+     */
+    fpController.fpdelegate = self;
+    
+    /*
+     * Ask for specific data types. (Optional) Default is all files.
+     */
+    fpController.dataTypes = [NSArray arrayWithObjects:@"text/plain", nil];
+    
+    /*
+     * Select and order the sources (Optional) Default is all sources
+     */
+    //fpController.sourceNames = [[NSArray alloc] initWithObjects: FPSourceImagesearch, nil];
+    
+    
+    /*
+     * Display it.
+     */
+    [self presentModalViewController:fpController animated:YES];
+}
+
+- (IBAction)savingAction: (id) sender {
+    NSLog(@"Saving");
+
+    /*
+     * Create the object
+     */
+    FPSaveController *fpSave = [[FPSaveController alloc] init];
+    
+    /*
+     * Set the delegate
+     */
+    fpSave.fpdelegate = self;
+    
+    /*
+     * Select and order the sources (Optional) Default is all sources
+     */
+    //fpSave.sourceNames = [[NSArray alloc] initWithObjects: FPSourceDropbox, FPSourceFacebook, FPSourceBox, nil];
+    //fpSave.sourceNames = [[NSArray alloc] initWithObjects: nil];
+    
+    /*
+     * Set the data and data type to be saved.
+     */
+    fpSave.data = [_textView.text dataUsingEncoding:NSUTF8StringEncoding] ;
+    fpSave.dataType = @"text/plain";
+    
+    /*
+     * Display it.
+     */
+    [self presentModalViewController:fpSave animated:YES];
+}
+
+#pragma mark - FPPickerControllerDelegate Methods
+
+- (void)FPPickerController:(FPPickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSLog(@"FILE CHOSEN: %@", info);
+    
+    _textView.text = [[NSString alloc] initWithContentsOfFile:[info valueForKey:@"FPPickerControllerReferenceURL"] encoding:NSUTF8StringEncoding error:nil];
+    [self dismissModalViewControllerAnimated:YES];
+    
+}
+- (void)FPPickerControllerDidCancel:(FPPickerController *)picker
+{
+    NSLog(@"FP Cancelled Open");
+    [self dismissModalViewControllerAnimated:YES];
+
+}
+
+#pragma mark - FPSaveControllerDelegate Methods
+
+- (void)FPSaveController:(FPSaveController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"FILE SAVED: %@", info);
+    [self dismissModalViewControllerAnimated:YES];
+}
+- (void)FPSaveControllerDidCancel:(FPSaveController *)picker {
+    NSLog(@"FP Cancelled Save");
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+@end
