0e807cb2c45b2ef56068f3f8299d1c4127f573f3
Ruben Nine <sonicbee9@gmx.com>
2014-06-07 00:53:27 +0200

    Started FPSaveSourceController cleanup

--- FPPicker/FPSaveSourceController.m
+++ FPPicker/FPSaveSourceController.m
@@ -17,11 +17,10 @@
 
 @implementation FPSaveSourceController
 
-@synthesize textField, saveButton;
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithNibName:nibNameOrNil
+                           bundle:nibBundleOrNil];
 
     if (self)
     {
@@ -34,38 +33,54 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+
+    UIBarButtonItem *flexibleSpace;
+
+    flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                  target:nil
+                                                                  action:nil];
 
     NSLog(@"Source: %@ Path: %@ %@", self.sourceType.identifier, self.path, [NSString stringWithFormat:@"%@/", self.sourceType.rootUrl]);
 
-    if ((self.sourceType.identifier == FPSourceFacebook || self.sourceType.identifier == FPSourcePicasa) && [self.path isEqualToString:[NSString stringWithFormat:@"%@/", self.sourceType.rootUrl]])
+    if ((self.sourceType.identifier == FPSourceFacebook ||
+         self.sourceType.identifier == FPSourcePicasa) &&
+        [self.path isEqualToString:[NSString stringWithFormat:@"%@/", self.sourceType.rootUrl]])
     {
         NSLog(@"SPECIAL");
+
         UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, 200, 21.0)];
-        [titleLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:18]];
-        [titleLabel setBackgroundColor:[UIColor clearColor]];
-        [titleLabel setTextColor:[UIColor whiteColor]];
-        [titleLabel setText:@"Choose an Album"];
-        [titleLabel setTextAlignment:NSTextAlignmentCenter];
+
+        titleLabel.font = [UIFont fontWithName:@"Helvetica-Bold"
+                                          size:18];
+
+        titleLabel.backgroundColor = [UIColor clearColor];
+        titleLabel.textColor = [UIColor whiteColor];
+        titleLabel.text = @"Choose an Album";
+        titleLabel.textAlignment = NSTextAlignmentCenter;
 
         UIBarButtonItem *title = [[UIBarButtonItem alloc] initWithCustomView:titleLabel];
-        [self setToolbarItems:[NSArray arrayWithObjects:flexibleSpace, title, flexibleSpace, nil]];
+
+        self.toolbarItems = @[flexibleSpace, title, flexibleSpace];
     }
     else
     {
         FPSaveController *fpsave = (FPSaveController *)self.fpdelegate;
 
-        self.saveButton = [[UIBarButtonItem alloc] initWithTitle:@"Save" style:UIBarButtonItemStyleDone target:self action:@selector(saveAction:)];
+        self.saveButton = [[UIBarButtonItem alloc] initWithTitle:@"Save"
+                                                           style:UIBarButtonItemStyleDone
+                                                          target:self
+                                                          action:@selector(saveAction:)];
 
         self.textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 210, 31)];
-        [textField setPlaceholder:@"filename"];
-        [textField setAutocorrectionType:UITextAutocorrectionTypeNo];
-        [textField setClipsToBounds:YES];
-        [textField setDelegate:self];
 
-        if (fpsave.proposedFilename != nil)
+        self.textField.placeholder = @"filename";
+        self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+        self.textField.clipsToBounds = YES;
+        self.textField.delegate = self;
+
+        if (fpsave.proposedFilename)
         {
-            textField.text = fpsave.proposedFilename;
+            self.textField.text = fpsave.proposedFilename;
         }
 
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 24)];
@@ -73,15 +88,15 @@
         label.text = [fpsave getExtensionString];
         label.textColor = [UIColor grayColor];
 
-        textField.rightViewMode = UITextFieldViewModeAlways;
-        textField.rightView = label;
-        [textField setContentVerticalAlignment:UIControlContentVerticalAlignmentCenter];
-        [textField setReturnKeyType:UIReturnKeyDone];
+        self.textField.rightViewMode = UITextFieldViewModeAlways;
+        self.textField.rightView = label;
+        self.textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+        self.textField.returnKeyType = UIReturnKeyDone;
+        self.textField.borderStyle = UITextBorderStyleRoundedRect;
 
-        [textField setBorderStyle:UITextBorderStyleRoundedRect];
-        UIBarButtonItem *filename = [[UIBarButtonItem alloc] initWithCustomView:textField];
+        UIBarButtonItem *filename = [[UIBarButtonItem alloc] initWithCustomView:self.textField];
 
-        [self setToolbarItems:[NSArray arrayWithObjects:flexibleSpace, filename, saveButton, nil]];
+        self.toolbarItems = @[flexibleSpace, filename, self.saveButton];
     }
 }
 
@@ -101,20 +116,39 @@
 {
     [super viewWillAppear:animated];
 
-    [self.navigationController setToolbarHidden:NO];
+    self.navigationController.toolbarHidden = NO;
 
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldDidChange:) name:UITextFieldTextDidChangeNotification object:self.textField];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(textFieldDidChange:)
+                                                 name:UITextFieldTextDidChangeNotification
+                                               object:self.textField];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
 
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UITextFieldTextDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UITextFieldTextDidChangeNotification
+                                                  object:nil];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -123,16 +157,16 @@
 
     if (fpsave.proposedFilename != nil)
     {
-        textField.text = fpsave.proposedFilename;
+        self.textField.text = fpsave.proposedFilename;
     }
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
-    if (textField.text != nil)
+    if (self.textField.text != nil)
     {
         FPSaveController *fpsave = (FPSaveController *)self.fpdelegate;
-        fpsave.proposedFilename = textField.text;
+        fpsave.proposedFilename = self.textField.text;
     }
 }
 
@@ -140,11 +174,15 @@
 {
     if (fpDEVICE_TYPE == fpDEVICE_TYPE_IPHONE)
     {
-        [UIView beginAnimations:nil context:NULL];
+        [UIView beginAnimations:nil
+                        context:NULL];
+
         [UIView setAnimationDuration:0.3];
 
         CGRect frame = self.navigationController.toolbar.frame;
-        frame.origin.y = frame.origin.y - 216;
+
+        frame.origin.y -= 216;
+
         self.navigationController.toolbar.frame = frame;
 
         [UIView commitAnimations];
@@ -155,11 +193,15 @@
 {
     if (fpDEVICE_TYPE == fpDEVICE_TYPE_IPHONE)
     {
-        [UIView beginAnimations:nil context:NULL];
+        [UIView beginAnimations:nil
+                        context:NULL];
+
         [UIView setAnimationDuration:0.3];
 
         CGRect frame = self.navigationController.toolbar.frame;
-        frame.origin.y = frame.origin.y + 216;
+
+        frame.origin.y += 216;
+
         self.navigationController.toolbar.frame = frame;
 
         [UIView commitAnimations];
@@ -168,30 +210,38 @@
 
 - (void)saveAction:(id)sender
 {
-    [self.fpdelegate FPSourceController:self didPickMediaWithInfo:[NSDictionary alloc]];
+    [self.fpdelegate FPSourceController:self
+                   didPickMediaWithInfo:nil];
 
     NSLog(@"Path %@", self.path);
-    FPSaveController *saveC = (FPSaveController *) self.fpdelegate;
-    [saveC saveFileName:self.textField.text To:self.path];
+
+    FPSaveController *saveC = (FPSaveController *)self.fpdelegate;
+
+    [saveC saveFileName:self.textField.text
+                     To:self.path];
 }
 
 - (void)objectSelectedAtIndex:(NSInteger)index
 {
-    NSMutableDictionary *obj = [self.contents objectAtIndex:index];
+    NSMutableDictionary *obj = self.contents[index];
 
-    if ([[NSNumber numberWithInt:1] isEqualToNumber:[obj valueForKey:@"is_dir"]])
+    if (YES == [obj[@"is_dir"] boolValue])
     {
-        FPSaveSourceController *subController = [[FPSaveSourceController alloc] init];
-        subController.path = [obj valueForKey:@"link_path"];
+        FPSaveSourceController *subController = [FPSaveSourceController new];
+
+        subController.path = obj[@"link_path"];
         subController.sourceType = self.sourceType;
         subController.fpdelegate = self.fpdelegate;
-        [self.navigationController pushViewController:subController animated:YES];
+
+        [self.navigationController pushViewController:subController
+                                             animated:YES];
 
         return;
     }
     else
     {
-        self.textField.text = [[obj valueForKey:@"filename"] stringByDeletingPathExtension];
+        self.textField.text = [obj[@"filename"] stringByDeletingPathExtension];
+
         [self textFieldDidChange:self.textField];
     }
 
@@ -221,14 +271,14 @@
 {
     if (self.sourceType.overwritePossible)
     {
-        //If it can overwite, warn the user.
+        // If it can overwite, warn the user.
 
         for (NSMutableDictionary *obj in self.contents)
         {
             FPSaveController *saveC = (FPSaveController *) self.fpdelegate;
             NSString* proposedName = [self.textField.text stringByAppendingString:[saveC getExtensionString]];
 
-            if ([[obj valueForKey:@"filename"] isEqualToString:proposedName])
+            if ([obj[@"filename"] isEqualToString:proposedName])
             {
                 self.saveButton.title = @"Overwrite";
 
@@ -238,24 +288,30 @@
                 }
 
                 CGRect frameRect = self.textField.frame;
+
                 frameRect.size.width = 183;
+
                 self.textField.frame = frameRect;
 
                 return;
             }
         }
 
-        //reset to default
+        // Reset to default
+
         self.saveButton.title = @"Save";
 
         if ([fpDEVICE_VERSION doubleValue] >= 5.0)
         {
             NSLog(@">=version5");
+
             self.saveButton.tintColor = nil;
         }
 
         CGRect frameRect = self.textField.frame;
+
         frameRect.size.width = 210;
+
         self.textField.frame = frameRect;
     }
 }
