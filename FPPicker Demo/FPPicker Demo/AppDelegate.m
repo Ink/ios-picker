b8b2e0f03b2d9c5e29766d28dded526f66aa4a87
Ruben Nine <sonicbee9@gmx.com>
2014-06-13 13:39:56 +0200

    Added -force_load to linker flags in demo project so our categories are seen (https://developer.apple.com/mac/library/qa/qa2006/qa1490.html). Updated FPPicker Demo to use storyboards and updated iPad storyboard.

--- FPPicker Demo/FPPicker Demo/AppDelegate.m
+++ FPPicker Demo/FPPicker Demo/AppDelegate.m
@@ -13,14 +13,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController"
-                                                           bundle:nil];
-
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
 
     return YES;
 }
