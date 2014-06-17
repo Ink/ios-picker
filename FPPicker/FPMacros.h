f0ef1947f1c58f263dc3aaa1a49a67b8fa25d159
Ruben Nine <sonicbee9@gmx.com>
2014-06-17 14:21:57 +0200

    Adding FPCLAMP macro

--- FPPicker/FPMacros.h
+++ FPPicker/FPMacros.h
@@ -29,3 +29,6 @@
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+#define FPCLAMP(x, minimum, maximum) \
+    MIN((maximum), MAX((minimum), (x)))
