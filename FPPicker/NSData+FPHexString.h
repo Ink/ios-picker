4c663c3bdaa3420d3df528552c67b9d5aa76bcc7
Ruben Nine <sonicbee9@gmx.com>
2014-07-14 18:17:45 +0200

    Adding NSData+FPHexString category

---
+++ FPPicker/NSData+FPHexString.h
@@ -0,0 +1,15 @@
+//
+//  NSData+FPHexString.h
+//  FPPicker
+//
+//  Created by Ruben Nine on 14/07/14.
+//  Copyright (c) 2014 Filepicker.io (Couldtop Inc.). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (FPHexString)
+
+- (NSString *)FPHexString;
+
+@end
---
+++ FPPicker/NSData+FPHexString.m
@@ -0,0 +1,26 @@
+//
+//  NSData+FPHexString.m
+//  FPPicker
+//
+//  Created by Ruben Nine on 14/07/14.
+//  Copyright (c) 2014 Filepicker.io (Couldtop Inc.). All rights reserved.
+//
+
+#import "NSData+FPHexString.h"
+
+@implementation NSData (FPHexString)
+
+- (NSString *)FPHexString
+{
+    NSMutableString *string = [NSMutableString stringWithCapacity:self.length * 2];
+    const unsigned char *dataBytes = self.bytes;
+
+    for (size_t idx = 0; idx < self.length; ++idx)
+    {
+        [string appendFormat:@"%02x", dataBytes[idx]];
+    }
+
+    return [string copy];
+}
+
+@end
