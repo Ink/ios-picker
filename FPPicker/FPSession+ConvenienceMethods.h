830e4af73a97c0b1c8e55704fa2db6e09a21c69d
Ruben Nine <sonicbee9@gmx.com>
2014-07-15 17:42:07 +0200

    Adding FPSession+ConvenienceMethods category with +sessionForFileUploads

---
+++ FPPicker/FPSession+ConvenienceMethods.h
@@ -0,0 +1,15 @@
+//
+//  FPSession+ConvenienceMethods.h
+//  FPPicker
+//
+//  Created by Ruben Nine on 15/07/14.
+//  Copyright (c) 2014 Filepicker.io (Couldtop Inc.). All rights reserved.
+//
+
+#import "FPSession.h"
+
+@interface FPSession (ConvenienceMethods)
+
++ (instancetype)sessionForFileUploads;
+
+@end
---
+++ FPPicker/FPSession+ConvenienceMethods.m
@@ -0,0 +1,37 @@
+//
+//  FPSession+ConvenienceMethods.m
+//  FPPicker
+//
+//  Created by Ruben Nine on 15/07/14.
+//  Copyright (c) 2014 Filepicker.io (Couldtop Inc.). All rights reserved.
+//
+
+#import "FPSession+ConvenienceMethods.h"
+#import "FPUtils.h"
+#import "FPConfig.h"
+
+@implementation FPSession (ConvenienceMethods)
+
++ (instancetype)sessionForFileUploads
+{
+    FPSession *fpSession = [FPSession new];
+
+    fpSession.APIKey = fpAPIKEY;
+
+    if (fpAPPSECRETKEY)
+    {
+        NSString *securityPolicy = [FPUtils policyForHandle:nil
+                                             expiryInterval:3600.0
+                                             andCallOptions:nil];
+
+        NSString *securitySignature = [FPUtils signPolicy:securityPolicy
+                                                 usingKey:fpAPPSECRETKEY];
+
+        fpSession.securityPolicy = securityPolicy;
+        fpSession.securitySignature = securitySignature;
+    }
+
+    return fpSession;
+}
+
+@end
