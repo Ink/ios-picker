3d4f116a4407ceee0b3f88fba2932d3fe28b8829
Ruben Nine <sonicbee9@gmx.com>
2014-08-13 13:48:10 +0200

    Added empty Mac project.

---
+++ FPPicker/Platforms/Mac/FPPicker_Mac.h
@@ -0,0 +1,13 @@
+//
+//  FPPicker_Mac.h
+//  FPPicker Mac
+//
+//  Created by Ruben Nine on 13/08/14.
+//  Copyright (c) 2014 Filepicker.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FPPicker_Mac : NSObject
+
+@end
---
+++ FPPicker/Platforms/Mac/FPPicker_Mac.m
@@ -0,0 +1,13 @@
+//
+//  FPPicker_Mac.m
+//  FPPicker Mac
+//
+//  Created by Ruben Nine on 13/08/14.
+//  Copyright (c) 2014 Filepicker.io. All rights reserved.
+//
+
+#import "FPPicker_Mac.h"
+
+@implementation FPPicker_Mac
+
+@end
