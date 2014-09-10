79dda2375e8b9cf59ccf4763c5384ad7e05fd6a8
Ruben Nine <sonicbee9@gmx.com>
2014-09-10 15:05:25 +0200

    Improving cell selection mechanism to prevent crashes.

--- FPPicker/Platforms/iOS/FPSourceController.m
+++ FPPicker/Platforms/iOS/FPSourceController.m
@@ -695,28 +695,36 @@ static const CGFloat ROW_HEIGHT = 44.0;
 {
     CGPoint tapPoint = [sender locationOfTouch:sender.view.tag
                                         inView:sender.view];
-
-    int rowIndex = (int)MIN(floor(tapPoint.x / 105), self.numPerRow - 1);
-
+    
+    int cellWidth = self.thumbSize + self.padding;
+    int rowIndex = (int)MIN(floor(tapPoint.x / cellWidth), self.numPerRow - 1);
+    
     // Do nothing if there isn't a corresponding image view.
-
-    if (rowIndex >= [sender.view.subviews count])
+    
+    if (rowIndex >= sender.view.subviews.count)
     {
         return;
     }
-
+    
     UIImageView *selectedView = sender.view.subviews[rowIndex];
+    
+    // Do nothing if image view has an invalid tag
+    
+    if (selectedView.tag <= 0)
+    {
+        return;
+    }
+    
     NSInteger index = selectedView.tag - CELL_FIRST_TAG;
-
     NSMutableDictionary *obj = self.contents[index];
     UIImage *thumbnail;
-
+    
     BOOL thumbExists = [obj[@"thumb_exists"] boolValue];
-
+    
     if (thumbExists)
     {
         thumbnail = selectedView.image;
-
+        
         [self objectSelectedAtIndex:index
                             forView:sender.view
                       withThumbnail:thumbnail];
