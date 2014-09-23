8ebfc6570d180f842a15ac89e5dd0d93f88d8772
Ruben Nine <sonicbee9@gmx.com>
2014-09-23 17:44:42 +0200

    typechange

--- FPPicker/Platforms/iOS/PublicHeaders/FPPicker/FPPicker.h
+++ FPPicker/Platforms/iOS/PublicHeaders/FPPicker/FPPicker.h
@@ -1,1 +1,13 @@
-../../FPPicker.h
+//
+//  FPPicker.h
+//  FPPicker
+//
+//  Created by Liyan David Chang on 6/20/12.
+//  Copyright (c) 2012 Filepicker.io. All rights reserved.
+//
+
+#import "FPConfig.h"
+#import "FPConstants.h"
+#import "FPExternalHeaders.h"
+#import "FPPickerController.h"
+#import "FPSaveController.h"
