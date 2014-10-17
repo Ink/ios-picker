062eb80703832dccd2889a4c427a38b96894a39c
Ruben Nine <sonicbee9@gmx.com>
2014-10-17 12:14:03 +0200

    Adding IB inspectable FPButton class with rollover state.

---
+++ FPPicker/Platforms/Mac/FPButton.h
@@ -0,0 +1,16 @@
+//
+//  FPButton.h
+//  FPPicker
+//
+//  Created by Ruben Nine on 17/10/14.
+//  Copyright (c) 2014 Filepicker.io. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+IB_DESIGNABLE
+@interface FPButton : NSButton
+
+@property (nonatomic, strong) IBInspectable NSImage *rolloverImage;
+
+@end
