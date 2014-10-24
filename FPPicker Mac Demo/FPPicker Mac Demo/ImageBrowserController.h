cad79fd1b1bd8177b150616100ba83c75a7869f6
Ruben Nine <sonicbee9@gmx.com>
2014-10-24 13:44:36 +0200

    Removing unused files from OS X demo project.

--- FPPicker Mac Demo/FPPicker Mac Demo/ImageBrowserController.h
+++
@@ -1,8 +0,0 @@
-//#import <Quartz/Quartz.h>
-//#import <Cocoa/Cocoa.h>
-//
-//@interface ImageBrowserController : NSWindowController
-//
-//@property (nonatomic, weak) IBOutlet IKImageBrowserView *imageBrowser;
-//
-//@end
